ee05d187af5c0fae9701389f71af698cdb685715
Ella Thomson <ellat@mymts.net>
2015-06-04 15:00:13 -0500

    Remove derivative calculations
    Receives the derivatives as an input so that there is no need to evaluate the derivatives at each iteration

--- SSA/genMeanVar.m
+++ SSA/genMeanVar.m
@@ -1,4 +1,4 @@
-function [tauPrime, a_0, aj] = genMeanVar (Rjs, V, X0, eis, gis)
+function [tauPrime, a_0, aj] = genMeanVar (Rjs, V, X0, eis, gis, all_rxns)
 
 %V = [-1 -1 1; -1 1 -1; 1 -2 0];
 %Rjs = [0 0 0];
@@ -11,28 +11,7 @@ species1 = X0(1); % amount of x1
 species2 = X0(2); % amount of x2
 species3 = X0(3); % amount of y
 
-syms x1; % symbolic variable for x1
-syms x2; % symbolic variable for x2
-syms y; % symbolic variable for y
-c1 = 2; % reaction rate of reaction 1
-c2 = 1; % reaction rate of reaction 2 
-c3 = 2; % reaction ate of reaction 3 
 
-nummat = zeros(numSpecies,numReactions); % blank matrix to hold f values
-changes = [-1, -1, +1;-1,1,-2;1,-1,0]; % changes in species quantities from reactions
-deriv={}; % blank cell to hold derivative symbolic function 
-
-syms f(x1,x2,y); %a for reaction 1 (symbolic)
-syms g(x1,x2,y); %a for reaction 2 (symbolic)
-syms h(x1,x2,y); %a for reaction 3 (symbolic) 
-
-f(x1,x2,y) = c1*x1*x2; % a for reaction 1
-g(x1,x2,y) = c2*x1*y; % a for reaction 2
-h(x1,x2,y) = (1/2)*c3*(x2*(x2-1)); % a for reaction 3
-
-all_species = [x1,x2,y]; % vector of all species involved in reaction 
-rxns = {f,g,h}; % 3 different function to represent 3 different a's
-all_rxns = [f,g,h]; % 3 different functions to represent 3 different a's 
 a_0 = single(sum(all_rxns(species1,species2,species3))); % a0 is sum of all aj's
 aj = single(all_rxns(species1,species2,species3));
 
