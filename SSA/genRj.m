a582d7ea4aa00b8774ff4e14e53fb74c46b73bcc
Ella Thomson <ellat@mymts.net>
2015-06-05 11:21:30 -0500

    update changes to SSATestTauBinomial

--- SSA/genRj.m
+++ SSA/genRj.m
@@ -1,4 +1,4 @@
-function [Rjs] = genRj (X0, V)
+function [Rjs] = genRj (X0, V, all_rxns)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % generates Ls values for each reaction in order to determine whether the
 % reaction is critical. If there are critical reactions, the function
@@ -6,51 +6,49 @@ function [Rjs] = genRj (X0, V)
 % function for critical reactions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% setup values
+
 % the minimum value of lj for rj to be considered a critical reaction. This
 % can be a whole number between 2-20. It's usually equal to 10
 nc = 10; 
 
-X0 = [40 40 20]; % amounts of each species (will eventaully be an input) 
+%X0 = [40 40 20]; % amounts of each species (will eventaully be an input) 
 
-V = [-1 -1 1; -1 1 -1; 1 -2 0]; % v values for each reaction 
+%V = [-1 -1 1; -1 1 -1; 1 -2 0]; % v values for each reaction 
 
 species1 = X0(1); % amount of x1
 species2 = X0(2); % amount of x2
 species3 = X0(3); % amount of y
 
-syms x1; % symbolic variable for x1
-syms x2; % symbolic variable for x2
-syms y; % symbolic variable for y
-c1 = 2; % reaction rate of reaction 1
-c2 = 1; % reaction rate of reaction 2 
-c3 = 2; % reaction ate of reaction 3 
 
-syms f(x1,x2,y); %a for reaction 1 (symbolic)
-syms g(x1,x2,y); %a for reaction 2 (symbolic)
-syms h(x1,x2,y); %a for reaction 3 (symbolic) 
+%syms x1; % symbolic variable for x1
+%syms x2; % symbolic variable for x2
+%syms y; % symbolic variable for y
+%c1 = 2; % reaction rate of reaction 1
+%c2 = 1; % reaction rate of reaction 2 
+%c3 = 2; % reaction ate of reaction 3 
 
-f(x1,x2,y) = c1*x1*x2; % a for reaction 1
-g(x1,x2,y) = c2*x1*y; % a for reaction 2
-h(x1,x2,y) = (1/2)*c3*(x2*(x2-1)); % a for reaction 3
+%syms f(x1,x2,y); %a for reaction 1 (symbolic)
+%syms g(x1,x2,y); %a for reaction 2 (symbolic)
+%syms h(x1,x2,y); %a for reaction 3 (symbolic) 
+
+%f(x1,x2,y) = c1*x1*x2; % a for reaction 1
+%g(x1,x2,y) = c2*x1*y; % a for reaction 2
+%h(x1,x2,y) = (1/2)*c3*(x2*(x2-1)); % a for reaction 3
 
 
-all_rxns = [f,g,h]; % 3 different functions to represent 3 different a's 
+%all_rxns = [f,g,h]; % 3 different functions to represent 3 different a's 
 
 % find ajs for each reaction and store in a vector
 aj = single(all_rxns(species1,species2,species3));
 
-<<<<<<< HEAD
-%% Check elements
-=======
-disp(all_rxns)
-disp(aj)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
->>>>>>> a6d36d7efb5791b51f1e1a794f8c436d3456ca5d
 % check_elements is used to keep track of which elements in the matrix v
-% will need to be checked to determine whether they are critical. If an aj is
+% will need to be check to determine whether they are critical. If an aj is
 % negative the entire row for that reaction will be zeroed. If Vij is
 % positive, then the one element will be zeroed. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 % Find which reactions have negative aj's, zero their whole row
 first_row = aj>0;
@@ -65,11 +63,9 @@ indexes = find(first_row);
 % find negative Vij's for reactions which are not zeroed out
 % negative Vij's will have a 1 in check_element, positive Vij's will have a
 % 0 in check_element
-check_elements([indexes], :) = V([indexes], :)<0;
+check_elements(indexes, :) = V(indexes, :)<0;
 
-%% Determine the critical reactions
-% ones in Rjs indicate that these reactions are critical reactions
-Rjs = zeros(1,3);
+
 
 % Stores the Lj value for all reactions
 Ljs = zeros(1,3);
@@ -84,7 +80,24 @@ for x = 1:3 % generates an lj value for each reaction
     V_to_check = Vs(ind); % finds vs of indexes determined above
     
     Ls = ceil(x_to_check ./ abs(V_to_check)); % find lj for all elements
-    Ljs(x) = min(Ls); % find lj for one reaction and store it (minimum Lj value)
+    if length(Ls) ~= 0
+        Ljs(x) = min(Ls); % find lj for one reaction and store it (minimum Lj value)
+    else
+        Ljs(x) = nc+1;
+    end
+
 end
 
 Rjs = single(Ljs < nc); % vector of critical reactions
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The goto variable will be an output that is used in the main SSA program
+% to determine whethe to go to the tau leap generation for critical
+% equations or for no critical equations. If it is 0 it means there are no
+% critical reactions, if it is 1 there are crtical equations. The rjs
+% values will also be outputed to detect which reactions are critical 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    
+
