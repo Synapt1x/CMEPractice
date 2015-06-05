299735e770b16f72321e87b53509454d54ee5f09
Ella Thomson <ellat@mymts.net>
2015-06-05 15:26:46 -0500

    Update DerivEvals
    Changes: Added commenting and removed the vector for all species (it was not used in the main program).
    Problems Remaining: Expanding the function to more than three reactions

--- SSA/derivEvals.m
+++ SSA/derivEvals.m
@@ -1,4 +1,9 @@
 function [all_rxns] = derivEvals ()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Uses symbolic variables to calculate and store the aj equations for all
+% reactions. It outputs the unsolved equations as symbolic equations stored
+% in a vector.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 syms x1; % symbolic variable for x1
 syms x2; % symbolic variable for x2
@@ -7,13 +12,15 @@ c1 = .02; % reaction rate of reaction 1
 c2 = .01; % reaction rate of reaction 2 
 c3 = .02; % reaction ate of reaction 3 
 
-syms f(x1,x2,y); %a for reaction 1 (symbolic)
-syms g(x1,x2,y); %a for reaction 2 (symbolic)
-syms h(x1,x2,y); %a for reaction 3 (symbolic) 
+% define three symbolic functions which will be used to evaluate the aj's
+% for each reaction (f for reaction 1, g for reaction 2, h for reaction 3)
+syms f(x1,x2,y); 
+syms g(x1,x2,y);
+syms h(x1,x2,y); 
 
-f(x1,x2,y) = c1*x1*x2; % a for reaction 1
-g(x1,x2,y) = c2*x1*y; % a for reaction 2
-h(x1,x2,y) = (1/2)*c3*(x2*(x2-1)); % a for reaction 3
+f(x1,x2,y) = c1*x1*x2; % a for reaction 1 (symbolic)
+g(x1,x2,y) = c2*x1*y; % a for reaction 2 (symbolic) 
+h(x1,x2,y) = (1/2)*c3*(x2*(x2-1)); % a for reaction 3 (symbolic) 
 
-all_species = [x1,x2,y]; % vector of all species involved in reaction 
 all_rxns = [f,g,h]; % 3 different functions to represent 3 different a's 
+
