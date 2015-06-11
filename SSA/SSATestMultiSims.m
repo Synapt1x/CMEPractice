fa5207c1df4bd59f615146a16da8cd281bdd6730
Ella Thomson <ellat@mymts.net>
2015-06-11 11:46:36 -0500

    Fix variance calculations, add commenting
    The variance calculations were previously taking the mean of all the variances in one interval. This was corrected to taking the sum of all variances in each interval, and then the mean of the variances from all intervals. 
    
    Commenting was added to explain the mean and variance calculations.

--- SSA/SSATestMultiSims.m
+++ SSA/SSATestMultiSims.m
@@ -18,7 +18,6 @@ max_rx = 100;
 % interval used for plotting means and calculating variance
 interval = 0.05 * max_rx;
 
-
 % evaluate derivatives for all equations. Returns a vector of 3 symbolic
 % equations (one for each reaction). Values will be plugged in to the
 % symbolic equations to calculate the aj for each reaction
@@ -39,7 +38,7 @@ for n = 1:num_sims % loop through all simulations. Plot after each sim
         % identify all critical reactions
         [Rjs, aj, a_0] = genRj (X0, V,nc, num_rx);
         
-        % generate one estimate for tau
+        % generate one estimate for tau (tau prime)
         [eis, gis] = genEis (0.05, V, X, num_species, num_rx);
         [tau_prime] = genMeanVar (Rjs, V, X0, eis, gis, tau_prime, aj, a_0, num_species);
         
@@ -56,6 +55,8 @@ for n = 1:num_sims % loop through all simulations. Plot after each sim
                     
                     Vj = V(j,:); % retrieve V values for the selected reaction
                     X0 = X0 + Vj; % get new X0 value
+                   
+                    % if species amount is less than 0, correct it
                     b = find(X0<0);
                     X0(b) = 0;
                     X = [X; X0]; % store all X values in a matrix
@@ -90,10 +91,12 @@ for n = 1:num_sims % loop through all simulations. Plot after each sim
                 % (only one critical reaction can occur)
                 [X0] = amountChangesDouble(X0, aj, V, tau, Rjs, num_rx);
                 time = time + tau; % find new time by adding tau to previous time
+                % if time is greater than the max time, correct it 
                 if time > max_rx
                     time = max_rx+0.1;
                 end
                 times = [times time]; % add new time to list of times
+                % if species amount is less than 0, correct it
                 b = find(X0<0);
                 X0(b) = 0;
                 X = [X; X0]; % store all X values in a matrix
@@ -101,82 +104,117 @@ for n = 1:num_sims % loop through all simulations. Plot after each sim
             end
         end   
     end
+    % all species amounts for one simulation
     XX = transpose(X);
+    
+    % store all times and species amounts for one simulations
     all_values_sim = [times; XX];
-    all_values = [all_values all_values_sim];
+    all_values = [all_values all_values_sim]; 
+    
+    % store all times and species amounts for all simulations 
     all_value_sim = [];
+    
+    % print the current simulation number (can be removed)
     disp('Current Simulation Number')
     disp(n)
     
 end
+
+% put all times and corresponding species amounts in ascending order 
 [~,I]=sort(all_values(1,:));
 B=all_values(:,I);
 
-times_average = B(1,:);
-x1_average = B(2,:);
-x2_average = B(3,:);
-y_average = B(4,:);
-z_average = B(5,:);
+times_average = B(1,:); % extract row with all times
+x1_average = B(2,:); % extract row with all X1 amounts
+x2_average = B(3,:); % extract row with all X2 amounts
+y_average = B(4,:); % extract row with all y amounts
+z_average = B(5,:); % extract row with all x amounts
 
 
 % calculations and plotting for moving average (mean)
 
+% vector to store time for each interval 
 mean_times = 0:interval:max_rx;
+% number of intervals to be used for mean calculation
 num_pts = length(mean_times);
+
+% blank vector to store x1 amounts at each interval
 mean_x1 = zeros(1, num_pts);
 mean_x1(1) = x1_average(1);
+
+% blank vector to store x2 amounts at each interval
 mean_x2 = zeros(1, num_pts);
 mean_x2(1) = x2_average(1);
+
+% blank vector to sore y amounts at each interval
 mean_y = zeros(1, num_pts);
 mean_y(1) = y_average(1);
+
+% blank vector to store z amounts at each interval
 mean_z = zeros(1, num_pts); 
 mean_z(1) = z_average(1); 
+
+% vector to store the current count of the for loop below 
 ints = [0];
 count = 1;
+
+% vector to store interval variances for x1
 variances_x1 = zeros(1, num_pts);
+
+% vector to store interval variances for x2
 variances_x2 = zeros(1, num_pts);
+
+% vector to store inerval variances for y
 variances_y = zeros(1, num_pts);
+
+% vector to store interval variances for z
 variances_z = zeros(1, num_pts);
-for int = interval:interval:max_rx
-    count = count+1;
-    ints = [ints int];
-    less = times_average > (ints(count-1));
-    more = times_average <= (ints(count));
-    bet = less.*more;
-    between = find(bet);
-    allx1s = x1_average(between);
-    allx2s = x2_average(between);
-    allys = y_average(between);
-    allzs = z_average(between);
-    num_between = length(allx1s);
+
+for int = interval:interval:max_rx % generates mean and variance for each interval
+    count = count+1; % increment the counter
+    ints = [ints int]; % store current count 
+    less = times_average > (ints(count-1)); % lower end of range 
+    more = times_average <= (ints(count)); % upper end of range
+    bet = less.*more; % will generate a 1 for all time indexes to be considered
+    between = find(bet); % find indexes of times to be considered
+    allx1s = x1_average(between); % all x1 amounts in the range
+    allx2s = x2_average(between); % all x2 amounts in the range
+    allys = y_average(between); % all y amounts in the range
+    allzs = z_average(between); % all z amounts in the range
+    
+    num_between = length(allx1s); % the number of times in the range
+    
     % calculations for means
-    mean_x1(count) = mean(allx1s);
-    mean_x2(count) = mean(allx2s);
-    mean_y(count) = mean(allys);
-    mean_z(count) = mean(allzs);
+    mean_x1(count) = mean(allx1s); % average of x1 amounts in the range
+    mean_x2(count) = mean(allx2s); % average of x2 amounts in the range
+    mean_y(count) = mean(allys); % average of y amounts in the range
+    mean_z(count) = mean(allzs); % average of z amounts in the range
     
     % calculations for variances
+    % the overall variance in each interval is the sum of all variances in
+    % that interval. The variances for x1, x2, y and z are calculated
+    % separately
     varsX1 = ((allx1s-mean_x1(count)).^2)./num_between;
-    variances_x1(count) = mean(varsX1);
+    variances_x1(count) = sum(varsX1); 
     
     varsX2 = ((allx2s-mean_x2(count)).^2)./num_between;
-    variances_x2(count) = mean(varsX2);
+    variances_x2(count) = sum(varsX2);
     
     varsY = ((allys-mean_y(count)).^2)./num_between;
-    variances_y(count) = mean(varsY);
+    variances_y(count) = sum(varsY);
     
     varsZ = ((allzs-mean_z(count)).^2)./num_between;
-    variances_z(count) = mean(varsZ);
+    variances_z(count) = sum(varsZ);
 end
 
 figure(2)
 
-% first plot displays x1 amount vs time
+% first plot displays average x1 amount vs time
 subplot(2,2,1)
-plot(times_average, x1_average, 'b')
+plot(times_average, x1_average, 'b') % plots all points from all simulations
 hold on
-plot(mean_times,mean_x1, 'k', 'LineWidth', 3)
-title('Average X1 Amount vs Time')
+plot(mean_times,mean_x1, 'k', 'LineWidth', 3) % plots mean point in each time interval
+title('Average X1 Amount vs Time (Time Intervals)')
 xlabel('Time')
 ylabel('X1 Amount')
 axis([0 inf 0 inf])
@@ -184,10 +222,10 @@ hold on
 
 % second plot displays x2 amount vs time
 subplot(2,2,2)
-plot(times_average, x2_average, 'r')
+plot(times_average, x2_average, 'r') % plots all points from all simulations
 hold on
-plot(mean_times, mean_x2, 'k', 'LineWidth', 3)
-title('Average X2 Amount vs Time')
+plot(mean_times, mean_x2, 'k', 'LineWidth', 3) % plots mean point in each interval
+title('Average X2 Amount vs Time (Time Intervals)') 
 xlabel ('Time')
 ylabel('X2 Amount')
 axis([0 inf 0 inf])
@@ -195,37 +233,37 @@ hold on
 
 % third plot displays y amount vs time
 subplot(2,2,3)
-plot(times_average ,y_average, 'g')
+plot(times_average ,y_average, 'g') % plots all points from all simulations
 hold on
-plot(mean_times, mean_y, 'k', 'LineWidth', 3)
-title('Y Amount vs Time')
+plot(mean_times, mean_y, 'k', 'LineWidth', 3) % plots average point in each interval
+title('Average Y Amount vs Time (Time Intervals)')
 xlabel('Time')
 ylabel('Y Amount')
 axis ([0 inf 0 inf])
 hold on
 
-% third plot displays y amount vs time
+% fourth plot displays y amount vs time
 subplot(2,2,4)
-plot(times_average ,z_average, 'c')
+plot(times_average ,z_average, 'c') % plots all points from all simulations
 hold on
-plot(mean_times, mean_z, 'k', 'LineWidth', 3)
-title('Z Amount vs Time')
+plot(mean_times, mean_z, 'k', 'LineWidth', 3) % plots average point in each interval
+title('Average Z Amount vs Time (Time Intervals)')
 xlabel('Time')
 ylabel('Z Amount')
 axis ([0 inf 0 inf])
 hold on
 
 
-disp('Variance X1')
-disp(mean(variances_x1))
+disp('Variance X1') % prints overall variance for x1
+disp(mean(variances_x1)) % variance x1 is mean of variances from each interval
 
-disp('Variance X2')
-disp(mean(variances_x2))
+disp('Variance X2') % prints overall vaiance for x2
+disp(mean(variances_x2)) % variance x2 is mean of variances from each interval
 
-disp('Variance Y')
-disp(mean(variances_y))
+disp('Variance Y') % prints overall variance for y
+disp(mean(variances_y)) % variance y is mean of variances from each interval
 
-disp('Variance Z')
-disp(mean(variances_z))
+disp('Variance Z') % prints overall variane for z
+disp(mean(variances_z)) % variance z is mean of variances from each interval
 
 toc
