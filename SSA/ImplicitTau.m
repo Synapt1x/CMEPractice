1cdb1523da198bc576c3c77962fc7218e3951b07
Ella Thomson <thomsoea@myumanitoba.ca>
2015-06-23 11:57:01 -0500

    Change number of species

--- SSA/ImplicitTau.m
+++ SSA/ImplicitTau.m
@@ -1,9 +1,9 @@
-function impTau = ImplicitTau(Rjs, V, aj, num_species, X, gis)
+function impTau = ImplicitTau(Rjs, V, aj, num_species, X0, gis)
 V(V>0) = 0; % zero out all product species
 V((Rjs>0), :) = 0; % zero out all critical reactions
 
-means = zeros(1, 3);
-vars = zeros(1, 3);
+means = zeros(1, num_species);
+vars = zeros(1, num_species);
 epsilon = 0.03;
 
 for (ii = 1: num_species)
@@ -11,9 +11,12 @@ for (ii = 1: num_species)
     vrow = transpose(vcol);
     tempMean = vrow .* aj; % find mean for one species
     tempVar = tempMean .* vrow; % find variance for one species
-    means(ii) = sum(tempMean); % store 
-    vars(ii) = sum(tempVar); 
+    means(ii) = sum(tempMean); % store mean for one species in a vector
+    vars(ii) = sum(tempVar);  % store variance for one spcies in a vector
 end
+
+X0(means==0)=0;
+topTerm = max(epsilon*X0 ./ gis);
 
 
 
